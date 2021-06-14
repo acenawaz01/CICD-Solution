56d98506517c8f59ee8a3c1e167cee1b652e05f7
acenawaz01 <46965770+acenawaz01@users.noreply.github.com>
2021-06-14 10:59:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import traceback
 
 app = Flask(__name__)
 
@@ -14,18 +15,31 @@ def main():
 
 @app.route('/send', methods=['POST'])
 def send():
-    if request.method == 'POST':
-        github_url = request.form['github_url']
-        github_credentials = request.form['github_credentials']
-        build_tool = request.form['build_tool']
-        meta_data_file = request.form['meta_data_file']
-        doSonarScan = request.form.getlist('sonar_scan')
-        doEC2deploy = request.form.getlist('ec2_deploy')
-
-    post_data = { JENKINS_USER : JENKINS_USER_TOKEN }
-    response = requests.post(JENKINS_URL, auth=(JENKINS_USER, JENKINS_USER_TOKEN),data = post_data)
-    result = response
-    return render_template('app.html',result=response)
+    try:
+        if request.method == 'POST':
+            github_url = request.form['github_url']
+            github_branch = request.form['github_branch']
+            github_credentials = request.form['github_credentials']
+            build_tool = request.form['build_tool']
+            doSonarScan = request.form.getlist('sonar_scan')
+            doEC2deploy = request.form.getlist('ec2_deploy')
+    except e:
+        traceback.print_exc()
+        result = status_code = -1
+    try:
+        my_data = {'Github_URL' : github_url, 'Github_Branch': github_branch, 'Github_Credentials' : github_credentials, 'Perform_Sonar_Scan' : doSonarScan , 'Perform_EC2_deployment' : doEC2deploy, 'build_tool' : build_tool}    
+        response = requests.post(JENKINS_URL, auth=(JENKINS_USER, JENKINS_USER_TOKEN), data = my_data)
+    except e:
+        traceback.print_exc()
+        result = status_code = -1
+        
+    result = response.status_code
+    
+    if (result is '201'):
+        result = "Job has been triggered successfully"
+    else:
+        result = "Error Encountered!! Please check logs"
+    return render_template('app.html',result=result)
 
 if __name__ == ' __main__':
     app.run(debug=True,host='0.0.0.0')
